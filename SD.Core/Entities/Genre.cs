5d9f64ba746897b3cef49e83b140a4253e11b57d
Horst Werner Schneider <horst.schneider@hotmail.com>
2026-04-01 15:55:26 +0200

    test

--- SD.Core/Entities/Genre.cs
+++ SD.Core/Entities/Genre.cs
@@ -6,7 +6,7 @@ namespace SD.Core.Entities
 {
     public class Genre
     {
-        public int Id { get; set; }
+        public int Id { get; set; }  
         public string Name { get; set; } = string.Empty;
 
     }
