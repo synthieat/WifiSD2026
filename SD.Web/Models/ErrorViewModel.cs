8dc6c87746a515113a8db5ce710513357155c75e
Horst Werner Schneider <horst.schneider@hotmail.com>
2026-05-28 21:33:09 +0200

    Added ASP.NET MVC project

---
+++ SD.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
