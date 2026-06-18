ddb47bc21908b40f77dbf742a43ffb6156661f1f
synthieat <horst.schneider@hotmail.com>
2026-06-18 20:23:05 +0200

    JQuery Globalize with CLDR

---
+++ SD.Web/wwwroot/js/globalize/jquery-globalize-de.js
@@ -0,0 +1,18 @@
+﻿$.when(
+$.getJSON("/lib/cldr-data/supplemental/likelySubtags.json"),
+$.getJSON("/lib/cldr-data/supplemental/numberingSystems.json"),
+$.getJSON("/lib/cldr-data/supplemental/timeData.json"),
+$.getJSON("/lib/cldr-data/supplemental/weekData.json"),
+$.getJSON("/lib/cldr-data/main/de/numbers.json"),
+$.getJSON("/lib/cldr-data/main/de/ca-gregorian.json"),
+$.getJSON("/lib/cldr-data/main/de/timeZoneNames.json"),
+).then(function () {
+    console.log("start slicing");
+return [].slice.apply(arguments, [0]).map(function (result) {
+    console.log("slicing done");
+return result[0];
+    });
+}).then(Globalize.load).then(function () {
+    Globalize.locale("de");
+console.log("Locale set to de");
+}).then(console.log("LOADED EVERYTHING"));
---
+++ SD.Web/wwwroot/js/globalize/jquery-globalize-en.js
@@ -0,0 +1,18 @@
+﻿$.when(
+    $.getJSON("/lib/cldr-data/supplemental/likelySubtags.json"),
+    $.getJSON("/lib/cldr-data/supplemental/numberingSystems.json"),
+    $.getJSON("/lib/cldr-data/supplemental/timeData.json"),
+    $.getJSON("/lib/cldr-data/supplemental/weekData.json"),
+    $.getJSON("/lib/cldr-data/main/en/numbers.json"),
+    $.getJSON("/lib/cldr-data/main/en/ca-gregorian.json"),
+    $.getJSON("/lib/cldr-data/main/en/timeZoneNames.json"),
+).then(function () {
+    console.log("start slicing");
+    return [].slice.apply(arguments, [0]).map(function (result) {
+        console.log("slicing done");
+        return result[0];
+    });
+}).then(Globalize.load).then(function () {
+    Globalize.locale("en");
+    console.log("Locale set to en");
+}).then(console.log("LOADED EVERYTHING"));
